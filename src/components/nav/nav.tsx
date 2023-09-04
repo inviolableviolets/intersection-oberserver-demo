b18bfed15423533a60704f452386fcc9d3bcc5da
Sergio Mata <sergiomataherrero@gmail.com>
2023-09-04 10:25:12 +0200

    Class 1

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,3 +1,26 @@
+import style from './nav.module.scss';
+import isdi from '../../assets/svg/isdi.svg';
+
 export function Nav() {
-  return <></>;
+  return (
+    <nav className={style['nav-container']}>
+      <main>
+        <img src={isdi} alt="ISDI Coders logo" />
+        <ul>
+          <li>
+            <a href="">About Us</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+          <li>
+            <a href="">Become a patreon</a>
+          </li>
+          <li>
+            <a href="">Login</a>
+          </li>
+        </ul>
+      </main>
+    </nav>
+  );
 }
